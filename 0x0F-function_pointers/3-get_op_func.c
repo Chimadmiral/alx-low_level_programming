d3c0013b6b479a5d37270793099856ee7eb5f151
Admiral <110990779+Chimadmiral@users.noreply.github.com>
2022-10-13 08:51:21 +0200

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,33 +1,31 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * get_op_func - determines which of the operations to use
- * @s: string parameter
- *
- * Return: pointer to operation
- */
+*get_op_func - selects the correct function to perform
+*the operation asked by the user. You’re not allowed
+*to declare any other function.
+*@s: operator passed as argument
+*
+*Return: A pointer to the function corresponding to
+*the operator given in parameter
+*/
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
 
-	i = 0;
-	while (ops[i].f != NULL)
-	{
-		if (*s == *(ops[i].op) && s[1] == '\0')
-			return (ops[i].f);
-		i++;
-	}
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
 
-	printf("Error\n");
-	exit(99);
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+
+return (ops[i].f);
+
 }
